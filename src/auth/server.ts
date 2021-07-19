fb1cd226e01927592a5ca52d2e1b64999edc5e5e
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-19 22:41:16 +1000

    🎉 Update

--- src/auth/server.ts
+++ src/auth/server.ts
@@ -5,7 +5,7 @@ import connectRedis from "connect-redis";
 
 const app = express();
 
-// Initialize Redis connection
+// Initialize Redis connection - https://youtu.be/mzG3tpZmRUE (Redis sessions) - https://youtu.be/jgpVdJB2sKQ (Redis tutorial)
 const RedisStore = connectRedis(session);
 const redisClient = redis.createClient({
     host: process.env.NODE_ENV !== "production" ? "localhost" : "redis",
@@ -18,9 +18,38 @@ app.use(
         store: new RedisStore({ client: redisClient }),
         secret: process.env.SECRET || "secret",
         saveUninitialized: false,
+        resave: false,
         cookie: {
             secure: process.env.NODE_ENV === "production",
             httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 7,
         },
     })
 );
+
+// Login endpoint
+app.post("/login", (req, res) => {
+    // Get params
+    const { email, password }: { email: string; password: string } = req.body;
+
+    // Store values in the session
+    // @ts-ignore
+    req.session.email = email;
+    // @ts-ignore
+    req.session.password = password;
+
+    // Return success
+    res.sendStatus(200);
+});
+
+// Protected route
+app.get("/protected", (req, res) => {
+    return res.json(req.session);
+});
+
+// Listen on specified port
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
