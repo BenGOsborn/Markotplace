826943ee8f755f2eebef9ffedc312eea4b0e634b
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-20 13:38:56 +1000

    🎉 Update

--- src/auth/utils/cache.ts
+++ src/auth/utils/cache.ts
@@ -1,7 +1,7 @@
 import { RedisClient } from "redis";
 
-// Cache data to redis
-const cacheData = async <T>(
+// Attempt to get the data from the cache if it exists
+const cacheData = <T>(
     redisClient: RedisClient,
     seconds: number,
     key: string,
@@ -9,9 +9,12 @@ const cacheData = async <T>(
 ) => {
     return new Promise<T>((resolve, reject) => {
         redisClient.get(key, async (error, cachedData) => {
+            // Reject if there was en error
             if (error) return reject(error);
+            // Return the cached data
             if (cachedData != null) resolve(JSON.parse(cachedData));
             else {
+                // Get the cached data from the callback, store it in the cache and return it
                 const freshData = await callback();
                 redisClient.setex(key, seconds, JSON.stringify(freshData));
                 resolve(freshData);
