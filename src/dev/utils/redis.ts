bc8bb9dc42c0fd7c1a6e99dbeaa9e04cc1fbe667
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-23 09:47:22 +1000

    🎉 Update

--- src/dev/utils/redis.ts
+++
@@ -1,7 +0,0 @@
-import redis from "redis";
-
-// Initialize and authorize with Redis
-export const redisClient = redis.createClient({
-    host: process.env.NODE_ENV !== "production" ? "localhost" : "redis",
-});
-redisClient.auth(process.env.REDIS_PASSWORD as string);
