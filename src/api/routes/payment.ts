b169e91d5012fb4f8e522d5eac39b62ad376fd1e
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-23 12:21:52 +1000

    🎉 Update

--- src/api/routes/payment.ts
+++ src/api/routes/payment.ts
@@ -4,7 +4,13 @@ import express from "express";
 const router = express.Router();
 
 // Allow a user to purchase an app
-router.post("/purchase", async (req, res) => {});
+router.post("/purchase", async (req, res) => {
+    // Get the details about the user
+    // How do I do this with the payment intents (or even charges ?)
+    // Transfer an amount to the user as well
+
+    res.sendStatus(200);
+});
 
 // Export the router
 export default router;
