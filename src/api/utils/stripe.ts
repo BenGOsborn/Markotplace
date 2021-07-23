13e779a7b625ea93550c64f6bb3052e6547c3f8d
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-23 11:21:50 +1000

    🎉 Update

--- src/api/utils/stripe.ts
+++ src/api/utils/stripe.ts
@@ -1,3 +1,8 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe();
+export const stripe = new Stripe(
+    (process.env.NODE_ENV === "production"
+        ? process.env.STRIPE_SECRET
+        : process.env.STRIPE_SECRET_TEST) as string,
+    { apiVersion: "2020-08-27" }
+);
