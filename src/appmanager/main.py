ff9e5c58c48bb391ef8f720bff67dbb00483910d
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-24 21:05:45 +1000

    🎉 Update

--- src/appmanager/main.py
+++ src/appmanager/main.py
@@ -1,88 +1,129 @@
 # https://github.com/rushter/socks5/blob/master/server.py
+# https://gist.github.com/okbash/b685ee4df9fb78832a8b4eda59fc7b64
 
 # Is this even needed ? Couldnt I just use some sort of premade reverse proxy to handle this ?
 
-import socket
-import threading
+import logging
 import select
+import socket
+import struct
+from socketserver import ThreadingMixIn, TCPServer, StreamRequestHandler
 
-
+logging.basicConfig(level=logging.DEBUG)
 SOCKS_VERSION = 5
 
 
-class Proxy:
-    def __init__(self):
-        self.username = "username"
-        self.password = "password"
+class ThreadingTCPServer(ThreadingMixIn, TCPServer):
+    pass
 
-    def handle_client(self, connection):
+
+class SocksProxy(StreamRequestHandler):
+    username = 'username'
+    password = 'password'
+
+    def handle(self):
+        logging.info('Accepting connection from %s:%s' % self.client_address)
+
         # greeting header
         # read and unpack 2 bytes from a client
-        version, nmethods = connection.recv(2)
+        header = self.connection.recv(2)
+        version, nmethods = struct.unpack("!BB", header)
 
-        # get available methods [0, 1, 2]
-        methods = self.get_available_methods(nmethods, connection)
+        # socks 5
+        assert version == SOCKS_VERSION
+        assert nmethods > 0
+
+        # get available methods
+        methods = self.get_available_methods(nmethods)
 
         # accept only USERNAME/PASSWORD auth
         if 2 not in set(methods):
             # close connection
-            connection.close()
+            self.server.close_request(self.request)
             return
 
         # send welcome message
-        connection.sendall(bytes([SOCKS_VERSION, 2]))
+        self.connection.sendall(struct.pack("!BB", SOCKS_VERSION, 2))
 
-        if not self.verify_credentials(connection):
+        if not self.verify_credentials():
             return
 
-        # request (version=5)
-        version, cmd, _, address_type = connection.recv(4)
+        # request
+        version, cmd, _, address_type = struct.unpack(
+            "!BBBB", self.connection.recv(4))
+        assert version == SOCKS_VERSION
 
         if address_type == 1:  # IPv4
-            address = socket.inet_ntoa(connection.recv(4))
+            address = socket.inet_ntoa(self.connection.recv(4))
         elif address_type == 3:  # Domain name
-            domain_length = connection.recv(1)[0]
-            address = connection.recv(domain_length)
+            domain_length = self.connection.recv(1)[0]
+            address = self.connection.recv(domain_length)
             address = socket.gethostbyname(address)
+        port = struct.unpack('!H', self.connection.recv(2))[0]
 
-        # convert bytes to unsigned short array
-        port = int.from_bytes(connection.recv(2), 'big', signed=False)
-
+        # reply
         try:
             if cmd == 1:  # CONNECT
                 remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 remote.connect((address, port))
                 bind_address = remote.getsockname()
-                print("* Connected to {} {}".format(address, port))
+                logging.info('Connected to %s %s' % (address, port))
             else:
-                connection.close()
+                self.server.close_request(self.request)
 
-            addr = int.from_bytes(socket.inet_aton(
-                bind_address[0]), 'big', signed=False)
+            addr = struct.unpack("!I", socket.inet_aton(bind_address[0]))[0]
             port = bind_address[1]
+            reply = struct.pack("!BBBBIH", SOCKS_VERSION, 0, 0, 1,
+                                addr, port)
 
-            reply = b''.join([
-                SOCKS_VERSION.to_bytes(1, 'big'),
-                int(0).to_bytes(1, 'big'),
-                int(0).to_bytes(1, 'big'),
-                int(1).to_bytes(1, 'big'),
-                addr.to_bytes(4, 'big'),
-                port.to_bytes(2, 'big')
-            ])
-        except Exception as e:
+        except Exception as err:
+            logging.error(err)
             # return connection refused error
             reply = self.generate_failed_reply(address_type, 5)
 
-        connection.sendall(reply)
+        self.connection.sendall(reply)
 
         # establish data exchange
         if reply[1] == 0 and cmd == 1:
-            self.exchange_loop(connection, remote)
+            self.exchange_loop(self.connection, remote)
 
-        connection.close()
+        self.server.close_request(self.request)
+
+    def get_available_methods(self, n):
+        methods = []
+        for i in range(n):
+            methods.append(ord(self.connection.recv(1)))
+        return methods
+
+    def verify_credentials(self):
+        version = ord(self.connection.recv(1))
+        assert version == 1
+
+        username_len = ord(self.connection.recv(1))
+        username = self.connection.recv(username_len).decode('utf-8')
+
+        password_len = ord(self.connection.recv(1))
+        password = self.connection.recv(password_len).decode('utf-8')
+
+        if username == self.username and password == self.password:
+            # success, status = 0
+            response = struct.pack("!BB", version, 0)
+            self.connection.sendall(response)
+            return True
+
+        # failure, status != 0
+        response = struct.pack("!BB", version, 0xFF)
+        self.connection.sendall(response)
+        self.server.close_request(self.request)
+        return False
+
+    def generate_failed_reply(self, address_type, error_number):
+        return struct.pack("!BBBBIH", SOCKS_VERSION, error_number, 0, address_type, 0, 0)
 
     def exchange_loop(self, client, remote):
+
         while True:
+
             # wait until client or remote is available for read
             r, w, e = select.select([client, remote], [], [])
 
@@ -96,57 +137,7 @@ class Proxy:
                 if client.send(data) <= 0:
                     break
 
-    def generate_failed_reply(self, address_type, error_number):
-        return b''.join([
-            SOCKS_VERSION.to_bytes(1, 'big'),
-            error_number.to_bytes(1, 'big'),
-            int(0).to_bytes(1, 'big'),
-            address_type.to_bytes(1, 'big'),
-            int(0).to_bytes(4, 'big'),
-            int(0).to_bytes(4, 'big')
-        ])
 
-    def verify_credentials(self, connection):
-        version = ord(connection.recv(1))  # should be 1
-
-        username_len = ord(connection.recv(1))
-        username = connection.recv(username_len).decode('utf-8')
-
-        password_len = ord(connection.recv(1))
-        password = connection.recv(password_len).decode('utf-8')
-
-        if username == self.username and password == self.password:
-            # success, status = 0
-            response = bytes([version, 0])
-            connection.sendall(response)
-            return True
-
-        # failure, status != 0
-        response = bytes([version, 0xFF])
-        connection.sendall(response)
-        connection.close()
-        return False
-
-    def get_available_methods(self, nmethods, connection):
-        methods = []
-        for i in range(nmethods):
-            methods.append(ord(connection.recv(1)))
-        return methods
-
-    def run(self, host, port):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((host, port))
-        s.listen()
-
-        print("* Socks5 proxy server is running on {}:{}".format(host, port))
-
-        while True:
-            conn, addr = s.accept()
-            print("* new connection from {}".format(addr))
-            t = threading.Thread(target=self.handle_client, args=(conn,))
-            t.start()
-
-
-if __name__ == "__main__":
-    proxy = Proxy()
-    proxy.run("127.0.0.1", 3000)
+if __name__ == '__main__':
+    with ThreadingTCPServer(('127.0.0.1', 3000), SocksProxy) as server:
+        server.serve_forever()
