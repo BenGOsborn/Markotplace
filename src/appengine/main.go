2be59b39e61bacdf23c491f24d698f03b177e206
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-25 10:53:08 +1000

    🎉 Update

--- src/appengine/main.go
+++ src/appengine/main.go
@@ -16,79 +16,77 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"time"
 
 	"appengine/containerutils"
 )
 
 // Initialize default values
 const PORT = 4000
-const STATE_COOKIE = "ctr.state"
+const STATE_COOKIE = "ctr.state.appname"
+
 var ctx context.Context = context.Background()
 var containers = []containerutils.Container{}
 
 func proxyHandler(w http.ResponseWriter, r *http.Request) {
 	// I also need some level of authentication and app checking here too
-	// How can I make admin verified state transfer ?
+	// How can I make admin verified state transfer ? (maybe I can have some type of admin password which gets shared between states via server secret ?)
 
-	// Only allow get requests
-	if r.Method != http.MethodGet {
-		fmt.Println("Called")
-		w.WriteHeader(400)
-		return
-	}
+	var appID string
 
-	// Print the state cookie if it exists
-	stateCookie, err := r.Cookie(STATE_COOKIE);
-	if err == nil {
-		fmt.Println(stateCookie)
-	}
-
-	// Get the AppID from the query
+	// Get the AppID from the query or the state cookie
 	appIDs, ok := r.URL.Query()["appID"]
-	if !ok || len(appIDs) < 1 { // Instead of sending an error, instead attempt to get the state cookie
-		w.WriteHeader(400)
-		return
-	}
-	appID := appIDs[0]
-
-	// Check if the specified path is valid
-	container, err := containerutils.GetContainer(ctx, appID, &containers)
-	if err != nil {
-		w.WriteHeader(500)
-		return
-	} else if container == nil {
-		w.WriteHeader(404)
-		return
-	}
-
-	// Initialize the forward URL
-	var forwardPort int
-
-	// Start the container if it does not exist, otherwise get the port for the container and log a hit
-	if !container.Active {
-		forwardPort = containerutils.GetPort()
-		if err := container.StartContainer(ctx, forwardPort); err != nil {
-			w.WriteHeader(500)
+	if !ok {
+		// Get the state cookie
+		stateCookie, err := r.Cookie(STATE_COOKIE)
+		if err != nil {
+			w.WriteHeader(400)
 			return
+		} else {
+			appID = stateCookie.Value
 		}
 	} else {
-		forwardPort = container.Port
-		container.LastHit = time.Now()
+		appID = appIDs[0]
 	}
+
+	fmt.Println(appID)
+
+	// // Check if the specified path is valid
+	// container, err := containerutils.GetContainer(ctx, appID, &containers)
+	// if err != nil {
+	// 	w.WriteHeader(500)
+	// 	return
+	// } else if container == nil {
+	// 	w.WriteHeader(404)
+	// 	return
+	// }
+
+	// // Initialize the forward URL
+	// var forwardPort int
+
+	// // Start the container if it does not exist, otherwise get the port for the container and log a hit
+	// if !container.Active {
+	// 	forwardPort = containerutils.GetPort()
+	// 	if err := container.StartContainer(ctx, forwardPort); err != nil {
+	// 		w.WriteHeader(500)
+	// 		return
+	// 	}
+	// } else {
+	// 	forwardPort = container.Port
+	// 	container.LastHit = time.Now()
+	// }
 
 	// Proxy pass to the correct route
 	remote, _ := url.Parse(fmt.Sprintf("http://0.0.0.0:%d", 3000))
-	proxy := httputil.NewSingleHostReverseProxy(remote);
+	proxy := httputil.NewSingleHostReverseProxy(remote)
 
 	// Set a cookie for maintaining the container connection
 	proxy.ModifyResponse = func(r *http.Response) error {
-		r.Header.Set("Set-Cookie", "appName=hahahaha")
+		r.Header.Set("Set-Cookie", fmt.Sprintf("%s=%s", STATE_COOKIE, appID))
 		return nil
 	}
 
 	proxy.ServeHTTP(w, r)
-} 
+}
 
 func main() {
 	// Start the container cleanup process
@@ -98,6 +96,6 @@ func main() {
 	http.HandleFunc("/", proxyHandler)
 
 	// Start the server and log error
-	log.Println(fmt.Sprintf("App engine listening on port %d...", PORT));
+	log.Println(fmt.Sprintf("App engine listening on port %d...", PORT))
 	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil))
 }
