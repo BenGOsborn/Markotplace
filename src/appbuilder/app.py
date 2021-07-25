9450c0c23b4acb3a92ad2702968ef879ee4b751a
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-25 15:10:23 +1000

    🎉 Update

--- src/appbuilder/app.py
+++ src/appbuilder/app.py
@@ -1,3 +1,6 @@
 # https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps (write:repo_hook) - when a user connects their repo, set a webhook in their account which will auto fire to my server on request
 # When a webhook is fired for a change to the deploy branch, we should download that repo, check the dockerfile for any problems, build the image, and then delete it
 # This should also be called on a successful app creation (ill have to integrate with my own server for that) AND we should have a manual deploy option for the app (user authenticated of course)
+# This will need access to the users dev account details, how will I integrate that ?
+
+from flask import Flask
