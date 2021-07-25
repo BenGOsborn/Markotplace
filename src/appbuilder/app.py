52d1f26b7dfff171c63631b4702f1a242491d252
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-25 15:15:59 +1000

    🎉 Update

--- src/appbuilder/app.py
+++ src/appbuilder/app.py
@@ -2,6 +2,7 @@
 # When a webhook is fired for a change to the deploy branch, we should download that repo, check the dockerfile for any problems, build the image, and then delete it
 # This should also be called on a successful app creation (ill have to integrate with my own server for that) AND we should have a manual deploy option for the app (user authenticated of course)
 # This will need access to the users dev account details, how will I integrate that ?
+# Set the deployment status as the current deployed branch on Markotplace
 
 # **** Im pretty sure that if a user gives full repo permissions I can write webhooks without asking for permission ? (YES IT DOES!)
 
