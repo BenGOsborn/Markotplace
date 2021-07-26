bd449ef6557ef3e3d26b5255168a97a205afe731
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-26 19:53:42 +1000

    🎉 Update

--- src/appbuilder/utils.py
+++ src/appbuilder/utils.py
@@ -1,14 +1,20 @@
 import psycopg2
 import os
 
+
 def is_safe():
     """
     Runs security checks on the downloaded repository to make sure that it is safe to be built.
     """
     pass
 
+
 def connect_db():
     """
     Establish connection to PostgreSQL server
     """
-    conn = psycopg2.connect(host="localhost", database="db", user="", password="")
+    # Load these from environment variables
+    conn = psycopg2.connect(
+        host="localhost", database="db", user="", password="")
+
+    return conn
