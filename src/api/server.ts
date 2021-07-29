4879990906185efcdf27727e7d6119c581628080
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-29 20:19:55 +1000

    🎉 Update

--- src/api/server.ts
+++ src/api/server.ts
@@ -25,7 +25,7 @@ app.use(
         saveUninitialized: false,
         resave: false,
         cookie: {
-            secure: process.env.ENVIRONMENT === "production",
+            // secure: process.env.ENVIRONMENT === "production",
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24 * 7,
         },
