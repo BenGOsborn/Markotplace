c54b333e9b6fe0b1e86e5ce0caa08d257c613c41
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-01 08:36:49 +1000

    🎉 Update

--- src/api/routes/apps.ts
+++ src/api/routes/apps.ts
@@ -1,7 +1,12 @@
+import axios from "axios";
 import express from "express";
 import { App } from "../entities/app";
+import { Dev } from "../entities/dev";
 import { User } from "../entities/user";
+import { cacheData, clearCache } from "../utils/cache";
+import { createAppSchema, editAppSchema } from "../utils/joiSchema";
 import { protectedMiddleware } from "../utils/middleware";
+import { stripe } from "../utils/stripe";
 
 // Initialize the router
 const router = express.Router();
@@ -95,6 +100,248 @@ router.get("/dev", protectedMiddleware, async (req, res) => {
 
     // Return the app data
     res.status(200).json({ apps });
+});
+
+// Add an app
+router.post("/app/create", async (req, res) => {
+    // Get the user data from the request
+    // @ts-ignore
+    const { user }: { user: User } = req.locals;
+
+    // Check that the user has a dev account
+    if (typeof user.dev === "undefined")
+        return res.status(400).send("A dev account is required");
+
+    // Get the data for the app
+    const {
+        name,
+        title,
+        description,
+        price,
+        ghRepoOwner,
+        ghRepoName,
+        ghRepoBranch,
+        env,
+    }: {
+        name: string;
+        title: string;
+        description: string;
+        price: number;
+        ghRepoOwner: string;
+        ghRepoName: string;
+        ghRepoBranch: string;
+        env: string;
+    } = req.body;
+
+    // Validate the app data
+    const { error } = createAppSchema.validate({
+        name,
+        title,
+        description,
+        price,
+        ghRepoOwner,
+        ghRepoName,
+        ghRepoBranch,
+        env,
+    });
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Check that an app with the same name does not exist
+    const exists = await App.findOne({
+        where: { name },
+    });
+    if (typeof exists !== "undefined")
+        return res.status(400).send("An app with that name already exists");
+
+    // Check that the dev has submitted their payment details if they wish to charge for their app
+    const detailsSubmitted = await cacheData(
+        `onboarded:${user.dev.id}`,
+        async () =>
+            (
+                await stripe.accounts.retrieve(user.dev.stripeConnectID)
+            ).details_submitted
+    );
+    if (!detailsSubmitted && price > 0)
+        return res
+            .status(400)
+            .send(
+                "To charge more than $0 for your app you must first finish setting up your Stripe account"
+            );
+
+    // Initialize a new webhook in the repository for the user
+    const {
+        data: { id: ghWebhookID },
+    } = await axios.post<{ id: string }>(
+        `https://api.github.com/repos/${ghRepoOwner}/${ghRepoName}/hooks`,
+        {
+            config: {
+                url: `${process.env.FRONTEND_URL}/appbuilder/hook`,
+                content_type: "json",
+            },
+        },
+        {
+            headers: {
+                Authorization: `token ${user.dev.ghAccessToken}`,
+                Accept: "application/vnd.github.v3+json",
+            },
+        }
+    );
+
+    // Create a new app and assign it to the dev account
+    const app = App.create({
+        name,
+        title,
+        description,
+        price: price * 100,
+        ghRepoOwner,
+        ghRepoName,
+        ghRepoBranch,
+        ghWebhookID,
+        env,
+    });
+    await app.save();
+
+    // Cache the new app
+    await cacheData(`app:${app.id}`, async () => app);
+
+    // Add the new app to the users account
+    let newUserApps: App[] = [app];
+    if (typeof user.apps !== "undefined")
+        newUserApps = [...user.apps, ...newUserApps];
+    await User.update(user.id, { apps: newUserApps });
+
+    // Add the new app to the users dev account
+    let newDevApps: App[] = [app];
+    if (typeof user.dev.apps !== "undefined")
+        newDevApps = [...user.dev.apps, ...newDevApps];
+    await Dev.update(user.dev.id, { apps: newDevApps });
+
+    // Clear the cached user
+    await clearCache(`user:${user.id}`);
+
+    // Add an app
+    res.sendStatus(200);
+});
+
+// Edit an app
+router.patch("/app/edit", async (req, res) => {
+    // Get the user data from the request
+    // @ts-ignore
+    const { user }: { user: User } = req.locals;
+
+    // Check that the user has a dev account
+    if (typeof user.dev === "undefined")
+        return res.status(400).send("A dev account is required");
+
+    // Get the data for the app
+    const {
+        name,
+        title,
+        description,
+        price,
+        ghRepoOwner,
+        ghRepoName,
+        ghRepoBranch,
+        env,
+    }: {
+        name: string;
+        title: string | undefined;
+        description: string | undefined;
+        price: number | undefined;
+        ghRepoOwner: string | undefined;
+        ghRepoName: string | undefined;
+        ghRepoBranch: string | undefined;
+        env: string | undefined;
+    } = req.body;
+
+    // Validate the edit app data
+    const { error } = editAppSchema.validate({
+        name,
+        title,
+        description,
+        price,
+        ghRepoOwner,
+        ghRepoName,
+        ghRepoBranch,
+        env,
+    });
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Find the app with the existing name
+    const existingApp = await App.findOne({ where: { name } });
+    if (typeof existingApp === "undefined")
+        return res.status(400).send("No app with this name exists");
+
+    // Check that the dev owns the app
+    if (existingApp.dev.id !== user.dev.id)
+        return res.status(403).send("You are not able to edit this app");
+
+    // Set the data to update
+    const updateData: any = {};
+    if (typeof title !== "undefined") updateData.title = title;
+    if (typeof description !== "undefined")
+        updateData.description = description;
+    if (typeof price !== "undefined") {
+        // Check that the dev has submitted their payment details if they wish to charge for their app
+        const detailsSubmitted = await cacheData(
+            `onboarded:${user.dev.id}`,
+            async () =>
+                (
+                    await stripe.accounts.retrieve(user.dev.stripeConnectID)
+                ).details_submitted
+        );
+        if (!detailsSubmitted && price > 0)
+            return res
+                .status(400)
+                .send(
+                    "To charge more than $0 for your app you must first finish setting up your Stripe account"
+                );
+
+        // Set the new price for the app
+        updateData.price = price * 100;
+    }
+    if (typeof ghRepoOwner !== "undefined")
+        updateData.ghRepoOwner = ghRepoOwner;
+    if (typeof ghRepoName !== "undefined") updateData.ghRepoName = ghRepoName;
+    if (typeof ghRepoBranch !== "undefined")
+        updateData.ghRepoBranch = ghRepoBranch;
+    if (typeof env !== "undefined") updateData.env = env;
+    if (
+        typeof ghRepoOwner !== "undefined" ||
+        typeof ghRepoName !== "undefined"
+    ) {
+        // Update the webhook if the repo was changed
+        const {
+            data: { id: ghWebhookID },
+        } = await axios.post<{ id: string }>(
+            `https://api.github.com/repos/${
+                ghRepoOwner || existingApp.ghRepoOwner
+            }/${ghRepoName || existingApp.ghRepoName}/hooks`,
+            {
+                config: {
+                    url: `${process.env.FRONTEND_URL}/appbuilder/hook`,
+                    content_type: "json",
+                },
+            },
+            {
+                headers: {
+                    Authorization: `token ${user.dev.ghAccessToken}`,
+                    Accept: "application/vnd.github.v3+json",
+                },
+            }
+        );
+        updateData.ghWebhookID = ghWebhookID;
+    }
+
+    // Update the app
+    await App.update(existingApp.id, updateData);
+
+    // Clear the cached data
+    await clearCache(`user:${user.id}`);
+    await clearCache(`app:${existingApp.id}`);
+
+    // Edit an app
+    res.sendStatus(200);
 });
 
 // Export the router
--- src/frontend/pages/user/dev/dashboard.tsx
+++ src/frontend/pages/user/dev/dashboard.tsx
@@ -1,18 +1,63 @@
 import axios from "axios";
 import { GetServerSideProps, NextPage } from "next";
 
-interface Props {}
+interface Props {
+    apps: {
+        name: string;
+        title: string;
+        description: string;
+        price: number;
+        ghRepoOwner: string;
+        ghRepoName: string;
+        ghRepoBranch: string;
+        env: string;
+    }[];
+}
 
-const Dashboard: NextPage<Props> = () => {
-    return <></>;
+const Dashboard: NextPage<Props> = ({ apps }) => {
+    return (
+        <>
+            {apps.length > 0 ? (
+                <div>
+                    {apps.map((app, index) => {
+                        return (
+                            <div key={index}>
+                                <h3>
+                                    <a href={app.name}>{app.title}</a>
+                                </h3>
+                                <p>{app.description}</p>
+                                <p>{app.price}</p>
+                                <p>{app.ghRepoOwner}</p>
+                                <p>{app.ghRepoName}</p>
+                                <p>{app.ghRepoBranch}</p>
+                                <p>{app.env}</p>
+                            </div>
+                        );
+                    })}
+                </div>
+            ) : (
+                <h3>No apps to display</h3>
+            )}
+        </>
+    );
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     try {
-        // Try and return the users data
-    } catch {}
-
-    return { props: {} as Props };
+        // Try and fetch a list of the users apps
+        const {
+            data: { apps },
+        } = await axios.get<Props>(`${process.env.BACKEND_URL}/api/apps/dev`, {
+            withCredentials: true,
+            headers: { Cookie: req.headers.cookie },
+        });
+        return { props: { apps } as Props };
+    } catch {
+        // Redirect to the settings page
+        res.statusCode = 302;
+        res.setHeader("Location", "/user/settings");
+        return { props: {} as Props };
+    }
 };
 
 // Export the page
