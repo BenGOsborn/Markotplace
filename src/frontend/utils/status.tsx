5458b475cd920a6f7bce34bb50b56f062a691bba
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-03 09:37:40 +1000

    🎉 Update

--- src/frontend/utils/status.tsx
+++ src/frontend/utils/status.tsx
@@ -7,13 +7,12 @@ export interface Status {
 export const StatusMessage = (props: { status: Status | null }) => {
     return (
         <>
-            {props.status ? (
-                props.status?.success ? (
+            {props.status &&
+                (props.status?.success ? (
                     <p className="textSuccess">{props.status?.message}</p>
                 ) : (
                     <p className="textFail">{props.status?.message}</p>
-                )
-            ) : null}
+                ))}
         </>
     );
 };
