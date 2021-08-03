83162ac307bb5d6ca6c49d69f7f0bd35645f54ed
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-03 22:57:48 +1000

    🎉 Update

--- src/apphandler/monitor/entry.go
+++ src/apphandler/monitor/entry.go
@@ -2,4 +2,5 @@ package monitor
 
 func Monitor() {
 	// Fetch the data from the database continuously for different apps, check the versions, and if different, rebuild them
+	// https://www.calhoun.io/connecting-to-a-postgresql-database-with-gos-database-sql-package/
 }
