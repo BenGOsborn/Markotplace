a8bf05cffd48b96bc38d120eb672f265b01b611e
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-03 22:51:39 +1000

    🎉 Update

--- src/apphandler/dev.Dockerfile
+++ src/apphandler/dev.Dockerfile
@@ -1,6 +1,13 @@
 # Build the environment
 FROM golang
 WORKDIR /usr/app
+RUN apt-get update && apt-get install -y \
+    software-properties-common \
+    npm
+RUN npm install npm@latest -g && \
+    npm install n -g && \
+    n latest
+RUN npm install -g nodemon
 
 # Start the server
-CMD go run main.go
+CMD nodemon --watch ../appmanager/ --ext '*' --signal SIGTERM --exec go run main.go
--- src/apphandler/main.go
+++ src/apphandler/main.go
@@ -6,17 +6,18 @@ import (
 	"net/http"
 )
 
+// Initialize constant variables
 const PORT = 5000
 
 func main() {
 	// **** CORS will ALSO be required for this to function properly
 
 	// Handle the main container redirect route
-	http.HandleFunc("/appmanager/reverse-proxy", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/apphandler", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello world!")
 	})
 
 	// Start the server and log error
-	log.Println(fmt.Sprintf("App manager listening on port %d...", PORT))
+	log.Println(fmt.Sprintf("Apphandler listening on port %d...", PORT))
 	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil))
 }
