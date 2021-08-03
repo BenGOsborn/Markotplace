d94e19ba57db11c4267a8c64a9638aee5f1d7690
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-03 22:55:59 +1000

    🎉 Update

--- src/apphandler/monitor/entry.go
+++ src/apphandler/monitor/entry.go
@@ -1,5 +1,5 @@
 package monitor
 
 func Monitor() {
-	// This should be responsible for monitoring the different apps
+	// Fetch the data from the database continuously for different apps, check the versions, and if different, rebuild them
 }
