277a661b3075c21f44caba1893f34594365c2756
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-03 23:09:21 +1000

    🎉 Update

--- src/apphandler/monitor/entry.go
+++ src/apphandler/monitor/entry.go
@@ -1,6 +1,7 @@
 package monitor
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 )
@@ -9,5 +10,11 @@ func Monitor() {
 	// Fetch the data from the database continuously for different apps, check the versions, and if different, rebuild them
 	// https://www.calhoun.io/connecting-to-a-postgresql-database-with-gos-database-sql-package/
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_HOST"), 5432, os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", os.Getenv("POSTGRES_HOST"), 5432, os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
 }
