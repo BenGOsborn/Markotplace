9d9f308edfc2221a2f2d4a2a78e28add0c84a15e
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-04 12:37:23 +1000

    🎉 Update

--- src/apphandler/docker/entry.go
+++ src/apphandler/docker/entry.go
@@ -1,7 +1,33 @@
 package docker
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
 
 func Docker() {
 	fmt.Println("Docker!")
+}
+
+func ListImages() ([]string, error) {
+	// Initialize Docker client
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, err
+	}
+
+	// Get a list of images from the host
+	images, err := cli.ImageList(nil, types.ImageListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract the names of the images and return them
+	names := []string{}
+	for _, image := range images {
+		names = append(names, image.ID)
+	}
+	return names, nil
 }
--- src/apphandler/go.mod
+++ src/apphandler/go.mod
@@ -1,3 +1,10 @@
 module apphandler
 
 go 1.16
+
+require (
+	github.com/containerd/containerd v1.5.5 // indirect
+	github.com/docker/docker v20.10.8+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	google.golang.org/grpc v1.39.0 // indirect
+)
