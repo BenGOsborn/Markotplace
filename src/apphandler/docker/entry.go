5ea6e32a661d309b14a360de7bfdbf2861541570
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-04 23:32:43 +1000

    🎉 Update

--- src/apphandler/docker/entry.go
+++ src/apphandler/docker/entry.go
@@ -1,6 +1,8 @@
 package docker
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"context"
 	"fmt"
 	"io"
@@ -95,10 +97,72 @@ func BuildImage(appName string) error {
 	filePath := filepath.Join(tempDir, "src.tar.gz")
 	file, _ := os.Create(filePath)
 	_, _ = io.Copy(file, resp.Body)
-	defer file.Close()
+	file.Close() // Try and defer this and keep the existing file stream
 
-	// Decompress
+	// Decompress **** figure out how this works!!! https://medium.com/@skdomino/taring-untaring-files-in-go-6b07cf56bc07
+	r, _ := os.Open(filePath)
+	gzr, err := gzip.NewReader(r)
+	if err != nil {
+		return err
+	}
+	defer gzr.Close()
+
+	tr := tar.NewReader(gzr)
+
+	for {
+		header, err := tr.Next()
+
+		switch {
+
+		// if no more files are found return
+		case err == io.EOF:
+			return nil
+
+		// return any other error
+		case err != nil:
+			return err
+
+		// if the header is nil, just skip it (not sure how this happens)
+		case header == nil:
+			continue
+		}
+
+		// the target location where the dir/file should be created
+		target := filepath.Join(tempDir, header.Name)
+
+		// the following switch could also be done using fi.Mode(), not sure if there
+		// a benefit of using one vs. the other.
+		// fi := header.FileInfo()
+
+		// check the file type
+		switch header.Typeflag {
+
+		// if its a dir and it doesn't exist create it
+		case tar.TypeDir:
+			if _, err := os.Stat(target); err != nil {
+				if err := os.MkdirAll(target, 0755); err != nil {
+					return err
+				}
+			}
+
+		// if it's a file create it
+		case tar.TypeReg:
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return err
+			}
+
+			// copy over contents
+			if _, err := io.Copy(f, tr); err != nil {
+				return err
+			}
+
+			// manually close here after each file operation; defering would cause each file close
+			// to wait until all operations have completed.
+			f.Close()
+		}
+	}
 
 	// Return no errors
-	return nil
+	// return nil
 }
