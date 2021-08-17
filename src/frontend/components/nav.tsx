e71d6d041887239d2c93bd2251c63eb499b9f643
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-17 22:52:38 +1000

    🎉 Update

--- src/frontend/components/nav.tsx
+++ src/frontend/components/nav.tsx
@@ -2,42 +2,61 @@ import axios from "axios";
 import Link from "next/link";
 import { useContext } from "react";
 import { authenticatedCtx } from "../utils/context";
+import styles from "../styles/Nav.module.scss";
 
 const Nav = () => {
     // Initialize the context
     const [isAuthenticated, setIsAuthenticated] = useContext(authenticatedCtx);
 
     return (
-        <>
-            <Link href="/">Home</Link>
-            <Link href="/apps">Market</Link>
-            {!isAuthenticated ? (
-                <>
-                    <Link href="/user/login">Login</Link>
-                    <Link href="/user/register">Register</Link>
-                </>
-            ) : (
-                <>
-                    <Link href="/">
-                        <a
-                            onClick={(e) =>
-                                axios
-                                    .post<string>(
-                                        `${process.env.BACKEND_URL}/api/user/logout`,
-                                        {},
-                                        { withCredentials: true }
-                                    )
-                                    .then((res) => setIsAuthenticated(false))
-                            }
-                        >
-                            Logout
-                        </a>
-                    </Link>
-                    <Link href="/user/library">Library</Link>
-                    <Link href="/user/settings">Settings</Link>
-                </>
-            )}
-        </>
+        <nav className={styles.nav}>
+            <span className={styles.logo}>
+                <Link href="/">Home</Link>
+            </span>
+            <ul>
+                <li>
+                    <Link href="/apps">Market</Link>
+                </li>
+                {!isAuthenticated ? (
+                    <>
+                        <li>
+                            <Link href="/user/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link href="/user/register">Register</Link>
+                        </li>
+                    </>
+                ) : (
+                    <>
+                        <li>
+                            <Link href="/">
+                                <a
+                                    onClick={(e) =>
+                                        axios
+                                            .post<string>(
+                                                `${process.env.BACKEND_URL}/api/user/logout`,
+                                                {},
+                                                { withCredentials: true }
+                                            )
+                                            .then((res) =>
+                                                setIsAuthenticated(false)
+                                            )
+                                    }
+                                >
+                                    Logout
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/user/library">Library</Link>
+                        </li>
+                        <li>
+                            <Link href="/user/settings">Settings</Link>
+                        </li>
+                    </>
+                )}
+            </ul>
+        </nav>
     );
 };
 
