546cae64517da60127d9ff9e6b490a3259d571ba
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-19 11:06:41 +1000

    🎉 Update

--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
+import styles from "../styles/Home.module.scss";
+
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <div className={styles.home}>
+            <header>
+                <h1>
+                    Deploy. <span>Monetize</span>. Fast. For free.
+                </h1>
+                <h2>
+                    Markotplace lets web app developers deploy and monetize
+                    their apps in under three minutes, all for free.
+                </h2>
+                <div className={styles.buttons}>
+                    <Link href="/dev">Get Started</Link>
+                    <Link href="/apps">Find Apps</Link>
+                </div>
+            </header>
+        </div>
+    );
 };
 
 // Export the page
