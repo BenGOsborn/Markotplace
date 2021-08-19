ac1e60b3aa526985ff51714dc1b571ec980b597c
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-19 22:59:23 +1000

    🎉 Update

--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -18,7 +18,9 @@ const Home = () => {
                 </div>
             </header>
             <div id="about" className={styles.content}>
-                <h1>For developers, by developers</h1>
+                <h1>
+                    For <span>developers</span>, by developers
+                </h1>
                 <div className={styles.blocks}>
                     <div className={styles.block}>
                         <p>
@@ -43,7 +45,36 @@ const Home = () => {
                 </div>
             </div>
             <div id="docker" className={styles.content}>
-                <h1>Deploy with Docker</h1>
+                <h1>
+                    Deploy with <span>Docker</span>
+                </h1>
+                <div className={styles.blocks}>
+                    <div className={styles.block}>
+                        <p>
+                            Lorem ipsum <a>dolor sit</a> amet consectetur
+                            adipisicing elit. Dignissimos quo voluptatibus
+                            quidem id dolore. Facilis, quis. Doloribus cumque
+                            labore est voluptatibus <a>reprehenderit</a>{" "}
+                            voluptas unde corrupti atque harum,{" "}
+                            <a>voluptatem</a> amet similique.
+                        </p>
+                    </div>
+                    <div className={styles.block}>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. <a>Dignissimos</a> quo voluptatibus quidem id
+                            dolore. Facilis, quis. Doloribus cumque labore est{" "}
+                            <a>voluptatibus</a> reprehenderit voluptas unde
+                            corrupti atque harum, voluptatem <a>amet</a>{" "}
+                            similique.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div id="play" className={styles.content}>
+                <h1>
+                    Use <span>apps</span> made by your favourite creators
+                </h1>
                 <div className={styles.blocks}>
                     <div className={styles.block}>
                         <p>
