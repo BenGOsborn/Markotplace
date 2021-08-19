26707f3ea0979b0d09ee4ff7feacadcc3204cb64
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-19 22:54:22 +1000

    🎉 Update

--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -17,7 +17,7 @@ const Home = () => {
                     <Link href="/apps">Find Apps</Link>
                 </div>
             </header>
-            <div id="developers" className={styles.content}>
+            <div id="about" className={styles.content}>
                 <h1>For developers, by developers</h1>
                 <div className={styles.blocks}>
                     <div className={styles.block}>
@@ -34,7 +34,32 @@ const Home = () => {
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. <a>Dignissimos</a> quo voluptatibus quidem id
-                            dolore. Facilis, quis. Doloribus cumque labore est
+                            dolore. Facilis, quis. Doloribus cumque labore est{" "}
+                            <a>voluptatibus</a> reprehenderit voluptas unde
+                            corrupti atque harum, voluptatem <a>amet</a>{" "}
+                            similique.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div id="docker" className={styles.content}>
+                <h1>Deploy with Docker</h1>
+                <div className={styles.blocks}>
+                    <div className={styles.block}>
+                        <p>
+                            Lorem ipsum <a>dolor sit</a> amet consectetur
+                            adipisicing elit. Dignissimos quo voluptatibus
+                            quidem id dolore. Facilis, quis. Doloribus cumque
+                            labore est voluptatibus <a>reprehenderit</a>{" "}
+                            voluptas unde corrupti atque harum,{" "}
+                            <a>voluptatem</a> amet similique.
+                        </p>
+                    </div>
+                    <div className={styles.block}>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. <a>Dignissimos</a> quo voluptatibus quidem id
+                            dolore. Facilis, quis. Doloribus cumque labore est{" "}
                             <a>voluptatibus</a> reprehenderit voluptas unde
                             corrupti atque harum, voluptatem <a>amet</a>{" "}
                             similique.
