3acd045f26c0a32cf85be74547345f7e2d79b110
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-21 08:15:13 +1000

    🎉 Update

--- src/frontend/components/footer.tsx
+++ src/frontend/components/footer.tsx
@@ -4,8 +4,20 @@ import styles from "../styles/Footer.module.scss";
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <div className={styles.links}>
-                <Link href="#">{`© Copyright Markotplace ${new Date().getFullYear()}`}</Link>
+            <div className={styles.rows}>
+                <div className={styles.links}>
+                    <Link href="#">{`© Copyright Markotplace ${new Date().getFullYear()}`}</Link>
+                    <Link href="#">{`© Copyright Markotplace ${new Date().getFullYear()}`}</Link>
+                    <Link href="#">{`© Copyright Markotplace ${new Date().getFullYear()}`}</Link>
+                </div>
+                <div className={styles.links}>
+                    <Link href="#">{`© Copyright Markotplace ${new Date().getFullYear()}`}</Link>
+                    <Link href="#">{`© Copyright Markotplace ${new Date().getFullYear()}`}</Link>
+                    <Link href="#">{`© Copyright Markotplace ${new Date().getFullYear()}`}</Link>
+                </div>
+                <div className={styles.links}>
+                    <Link href="#">{`© Copyright Markotplace ${new Date().getFullYear()}`}</Link>
+                </div>
             </div>
         </footer>
     );
