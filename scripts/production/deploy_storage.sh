0a4a631b32ddaca04d1f55ce88e6675df8f286c3
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-22 19:30:49 +1000

    🎉 Update

--- scripts/production/deploy_storage.sh
+++ scripts/production/deploy_storage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Create the data directories if they don't exist
-mkdir -p ../../data/redis
-mkdir -p ../../data/postgres
+mkdir -p data/redis
+mkdir -p data/postgres
 
 # Start storage services
-docker-compose -f ../../docker-compose/docker-compose.storage.yml --env-file ../../env/.env.production up
+docker-compose -f docker-compose/docker-compose.storage.yml --env-file env/.env.production up
