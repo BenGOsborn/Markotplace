eba7657d8330cfb860547f36b83e0a8cf397aca0
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-22 19:09:03 +1000

    🎉 Update

--- scripts/dev/deploy.sh
+++ scripts/dev/deploy.sh
@@ -2,3 +2,15 @@
 
 # Start the dev Docker Compose for the app
 docker-compose -f ./docker-compose/docker-compose.dev.yml --env-file ./env/.env.dev up
+
+while getopts ":b" option; do
+    case $option in
+        b)
+            # Build the containers and start them
+            docker-compose -f ./docker-compose/docker-compose.dev.yml --env-file ./env/.env.dev up --build
+            exit;;
+    esac 
+done
+
+# Start the containers
+docker-compose -f ./docker-compose/docker-compose.dev.yml --env-file ./env/.env.dev up
