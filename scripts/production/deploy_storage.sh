c0d4deea99ff3d6cc4b053a50e9f61020f2aeb3d
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-22 16:06:02 +1000

    🎉 Update

--- scripts/production/deploy_storage.sh
+++ scripts/production/deploy_storage.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# How to load env variables into the script ?
+# Start Redis
 docker run -dp 6379:6379 --name redis redis redis-server --requirepass ${REDIS_PASSWORD}
+
+# Start PostgreSQL
+docker run -dp 5432:5432 --name db -e POSTGRES_USER=${POSTGRES_USER} -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} -e POSTGRES_DB=${POSTGRES_DB} postgres
