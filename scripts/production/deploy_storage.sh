9ccee564734e10354d4d4bdd602d4eccece3ed9e
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-22 18:34:04 +1000

    🎉 Update

--- scripts/production/deploy_storage.sh
+++ scripts/production/deploy_storage.sh
@@ -4,8 +4,5 @@
 mkdir -p ./data/redis
 mkdir -p ./data/postgres
 
-# Start Redis
-docker run -dp 6379:6379 -v ./data/redis:/data --name redis redis redis-server --requirepass ${REDIS_PASSWORD}
-
-# Start PostgreSQL
-docker run -dp 5432:5432 -v ./data/postgres:/data --name postgres -e POSTGRES_USER=${POSTGRES_USER} -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} -e POSTGRES_DB=${POSTGRES_DB} postgres
+# Start storage services
+docker-compose -f ./docker-compose/docker-compose.storage.yml --env-file ./env/.env.production up
