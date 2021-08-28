fc8723bb06555178a9c92ea094d2c1821cfb5a7d
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-08-28 18:55:54 +1000

    🎉 Update

--- src/frontend/pages/user/dev/dashboard.tsx
+++ src/frontend/pages/user/dev/dashboard.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { GetServerSideProps, NextPage } from "next";
+import Head from "next/head";
 import Link from "next/link";
 import Card from "../../../components/card";
 import styles from "../../../styles/Dashboard.module.scss";
@@ -21,43 +22,54 @@ interface Props {
 
 const Dashboard: NextPage<Props> = ({ apps, url, onboarded }) => {
     return (
-        <div className={styles.dashboard}>
-            <nav>
-                {onboarded ? (
-                    <a href={url}>View your Stripe dashboard</a>
-                ) : (
-                    <a href={url}>Connect with Stripe to monetize your apps</a>
-                )}
-                <Link href="/user/dev/authorize-github">
-                    Reconnect GitHub account
-                </Link>
-                <Link href="/user/dev/apps/create">
-                    <a>
-                        <span>New app</span>
-                    </a>
-                </Link>
-            </nav>
-            <div className={styles.description}>
-                <h1>Your apps</h1>
-                <h2>Select an app to edit it</h2>
-            </div>
-            {apps.length > 0 ? (
-                <div className={styles.grid}>
-                    {apps.map((app, index) => {
-                        return (
-                            <Card
-                                key={index}
-                                title={app.title}
-                                description={app.description}
-                                link={`/user/dev/apps/edit/${app.name}`}
-                            />
-                        );
-                    })}
+        <>
+            <Head>
+                <title>Dashboard - Markotplace</title>
+                <meta
+                    name="description"
+                    content="Update your existing deployed apps or deploy a new app."
+                />
+            </Head>
+            <div className={styles.dashboard}>
+                <nav>
+                    {onboarded ? (
+                        <a href={url}>View your Stripe dashboard</a>
+                    ) : (
+                        <a href={url}>
+                            Connect with Stripe to monetize your apps
+                        </a>
+                    )}
+                    <Link href="/user/dev/authorize-github">
+                        Reconnect GitHub account
+                    </Link>
+                    <Link href="/user/dev/apps/create">
+                        <a>
+                            <span>New app</span>
+                        </a>
+                    </Link>
+                </nav>
+                <div className={styles.description}>
+                    <h1>Your apps</h1>
+                    <h2>Select an app to edit it</h2>
                 </div>
-            ) : (
-                <h3>No apps to display</h3>
-            )}
-        </div>
+                {apps.length > 0 ? (
+                    <div className={styles.grid}>
+                        {apps.map((app, index) => {
+                            return (
+                                <Card
+                                    key={index}
+                                    title={app.title}
+                                    description={app.description}
+                                    link={`/user/dev/apps/edit/${app.name}`}
+                                />
+                            );
+                        })}
+                    </div>
+                ) : (
+                    <h3>No apps to display</h3>
+                )}
+            </div>
+        </>
     );
 };
 
