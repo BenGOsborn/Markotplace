a46ac1388df73cc343d1f9877dd8b6979e3b54fc
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-10-04 10:40:07 +1100

    🎉 Update

--- src/api/utils/db.ts
+++ src/api/utils/db.ts
@@ -8,6 +8,7 @@ export const connectDB = () => {
     createConnection({
         type: "postgres",
         host: process.env.POSTGRES_HOST,
+        port: parseInt(process.env.POSTGRES_PORT as string),
         database: process.env.POSTGRES_DB,
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
--- src/api/utils/redis.ts
+++ src/api/utils/redis.ts
@@ -3,5 +3,6 @@ import redis from "redis";
 // Initialize and authorize with Redis
 export const redisClient = redis.createClient({
     host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT as string),
 });
 redisClient.auth(process.env.REDIS_PASSWORD as string);
