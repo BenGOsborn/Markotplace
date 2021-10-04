037149ac1a0ba1633170a6b9e23013a24b2adbe2
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-10-04 19:43:40 +1100

    🎉 Update

--- scripts/deploy/production/stop.sh
+++ scripts/deploy/production/stop.sh
@@ -1,2 +1,2 @@
-docker-compose -f docker-compose/docker-compose.production.yml down
-docker-compose -f docker-compose/docker-compose.storage.yml down
+docker-compose -f docker-compose/docker-compose.production.yml --env-file env/.env.production down
+docker-compose -f docker-compose/docker-compose.storage.yml --env-file env/.env.production down
