b1343d1900dd765cdcf6136230dfe449f18d8c4d
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-10-05 22:39:42 +1100

    🎉 Update

--- scripts/deploy/production/deploy_ssl.sh
+++ scripts/deploy/production/deploy_ssl.sh
@@ -3,6 +3,7 @@
 # Load env file
 export $(cat env/.env.production | xargs)
 echo "============== THIS IS THE ADDRES =========="
+cat env/.env.production
 echo ${FRONTEND_ADDRESS}
 echo "============== THIS IS THE ADDRES =========="
 
